b0915b7fd6755502e868b6c64220d50997d98fac
mevnik <mevnik@gmail.com>
2023-08-18 16:31:21 +0200

    test await

--- assets.js
+++ assets.js
@@ -8,13 +8,14 @@ exports.startGame = async function(ctx,gameOptions,numbers){
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+}
+/*
   numbers[ctx.chat.id] = Math.floor(Math.random() * 10)
       console.log('ID:',ctx.chat.id,'value:',numbers[ctx.chat.id])
       return ctx.reply("Let's play.\n What's my number?",
        gameOptions
       )
 }
-/*
 
 const{connection} = require('./db.js')
 
